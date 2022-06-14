83e45a5fba8ea0464220e330c30d5c7fd93c6c7c
Reagan Bodily <reaganbodily@gmail.com>
2022-06-13 20:46:12 -0600

    added reminder to update resume after end of course

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,7 +7,7 @@ function Home() {
             <div>
                 <h1 className="text-md sm:text-7xl text-light">Welcome to My Portfolio!</h1>
             </div>
-            {/* <p>update the resume in your anchor tag you asshole</p> */}
+            {/* <p>update the resume in your anchor tag</p> */}
             <div className="w-100 flex-row justify-center">
                 <a className="text-light" target="_blank" rel='noreferrer' href={resume}>
                     <h3 className="text-tertiary" >Take a look at my Resume</h3>
